4d7cb3664432d03fb3f4bd02b17cf376f7ffad1e
Nacho <lachezar.zah@gmail.com>
2024-04-30 10:57:21 +0300

    Add GlobalServerBasedSolution

--- app/components/GlobalServerBasedSolution/index.tsx
+++ app/components/GlobalServerBasedSolution/index.tsx
@@ -1,5 +1,16 @@
+import styles from './gsbs.module.scss';
+
 function GlobalServerBasedSolution() {
-  return <></>
+  return <div className={styles.globalServerBasedSolution}>
+    <div className={styles.gsbsTitle}>Our Global Server Based Solution</div>
+
+    <div className={styles.gsbsShowcase}>
+      <img className={styles.gsbsImg} src='https://fastly.picsum.photos/id/1047/1440/500.jpg?hmac=4eTDGIccoOYQ2cjPfwQK3J5gGLhJIWytIcdamhpYhMU'/>
+      <div className={styles.gsbsShowcaseTitle}>OCTOPUS</div>
+      <div className={styles.gsbsShowcaseSubtitle}>Server Based Gaming</div>
+      <div className={styles.gsbsShowcaseCircle}/>
+    </div>
+  </div>
 }
 
 export default GlobalServerBasedSolution;
