88b79fac3b76d7ba276fc0408df3d99d5975792b
Nacho <lachezar.zah@gmail.com>
2024-04-30 10:15:16 +0300

    Add Product Lines

--- app/components/ProductLines/index.tsx
+++ app/components/ProductLines/index.tsx
@@ -1,5 +1,27 @@
+import styles from './productLines.module.scss';
+
 function ProductLines() {
-  return <></>
+  return <div className={styles.productLines}>
+    <div className={styles.productLinesTitle}>Our Products</div>
+
+    <div className={styles.productLinesWrapper}>
+      <div className={styles.productLine}>
+        <img src='https://fastly.picsum.photos/id/698/300/500.jpg?hmac=0Dj0rhPBwjjhC9zDlBK2jPRwzDbZsPc7AEu9f6pj3YA'/>
+        <div className={styles.productLineName}>R-LINE</div>
+        <div className={styles.productLineCircle}/>
+      </div>
+      <div className={styles.productLine}>
+        <img src='https://fastly.picsum.photos/id/698/300/500.jpg?hmac=0Dj0rhPBwjjhC9zDlBK2jPRwzDbZsPc7AEu9f6pj3YA'/>
+        <div className={styles.productLineName}>S-LINE</div>
+        <div className={styles.productLineCircle}/>
+      </div>
+      <div className={styles.productLine}>
+        <img src='https://fastly.picsum.photos/id/698/300/500.jpg?hmac=0Dj0rhPBwjjhC9zDlBK2jPRwzDbZsPc7AEu9f6pj3YA'/>
+        <div className={styles.productLineName}>T-LINE</div>
+        <div className={styles.productLineCircle}/>
+      </div>
+    </div>
+  </div>
 }
 
 export default ProductLines;
